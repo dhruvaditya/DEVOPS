27e00adcd16a7349f798816c3839759d0867d4a3
Aditya Raj <89244720+dhruvaditya@users.noreply.github.com>
2023-09-29 15:11:26 +0530

    Made necessary changes for E-Waste Facility locator

--- lib/Modal/courses_main_page.dart
+++ lib/Modal/courses_main_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 class Programming extends StatefulWidget {
   const Programming({Key? key}) : super(key: key);
@@ -11,7 +13,55 @@ class _ProgrammingState extends State<Programming> {
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: Drawer(
-
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                // color: Colors.blue,
+                // Image(),
+                color: Colors.greenAccent,
+              ),
+              child: Text('Hi Satyam ! Sell your E-Waste',),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: const Text('Home'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.info_rounded),
+              title: const Text('About Us'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.book_outlined),
+              title: const Text('Insights about E-waste'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.logout_outlined),
+              title: const Text('Log Out'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
+        ),
       ),
       appBar: AppBar(title: Text("E - Waste Facility Locator"),backgroundColor: Color.fromRGBO(0, 0, 128, 0.7),),
       body: Column(
@@ -76,23 +126,29 @@ class _ProgrammingState extends State<Programming> {
                   debugPrint('Card Tapped');
                 },
                 child: Container(
-                  decoration: BoxDecoration(
-    borderRadius: BorderRadius.circular(20),
-                    gradient: LinearGradient(
-                      colors: [Colors.blueAccent, Colors.lightBlue.shade100], // Adjust colors as needed
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
+                  child: Card(
+                    child: InkWell(
+                      splashColor: Colors.green.withAlpha(30),
+                      onTap: () {
+                        debugPrint('Card Tapped');
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          gradient: LinearGradient(
+                            colors: [Colors.green, Colors.greenAccent.shade100], // Adjust colors as needed
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                          ),
+                        ),
+                        width: 380,
+                        height: 201,
+                        child: Center(
+                          child: Text('Insight about E- Waste', style: TextStyle(color: Colors.white)),
+                        ),
+                      ),
                     ),
                   ),
-                  width: 380,
-                  height: 201,
-                  child: Column(
-                    children: [
-                      Center(
-                        child: Text("Insight about E- Waste",),
-                      )
-                    ],
-                  )
                 ),
               ),
             ),
